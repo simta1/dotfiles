beb7cc058bbaaaf5e8ef74d21205465900023efc
simta1 <koreajunho0219@gmail.com>
2025-05-11 17:51:37 +0900

    add zshrc

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
+# dotfiles
 ```bash
 git clone https://github.com/simta1/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 chmod +x install.sh
 ./install.sh
 ```
-이후 nvim 열어서
-```nvim
-:PlugInstall
-```
+
+### 포함되어있는 설정파일들
+- **Neovim (`~/.config/nvim/init.vim`)**
+  - nvim 실행해서 `:PlugInstall`로 플러그인 설치
+
+- **Zsh (`~/.zshrc`)**
--- install.sh
+++ install.sh
@@ -2,15 +2,16 @@
 
 DOTFILES_DIR="$HOME/dotfiles"
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
-mkdir -p "$NVIM_CONFIG_DIR"
 
+# ~/.config/nvim/init.vim
+mkdir -p "$NVIM_CONFIG_DIR"
 if [ -f "$NVIM_CONFIG_DIR/init.vim" ]; then
     echo "기존 init.vim 백업 → init.vim.backup"
     mv "$NVIM_CONFIG_DIR/init.vim" "$NVIM_CONFIG_DIR/init.vim.backup"
 fi
 
 ln -sfn "$DOTFILES_DIR/.config/nvim/init.vim" "$NVIM_CONFIG_DIR/init.vim"
-echo "init.vim 심볼릭 링크 생성 완료"
+echo "init.vim 심볼릭 링크 완료"
 
 PLUG_VIM_PATH="$HOME/.local/share/nvim/site/autoload/plug.vim"
 if [ ! -f "$PLUG_VIM_PATH" ]; then
@@ -24,3 +25,10 @@ fi
 
 echo "Neovim 열어서 :PlugInstall으로 플러그인 설치"
 
+# ~/.zshrc
+if [ -f "$HOME/.zshrc" ]; then
+    echo "기존 .zshrc 백업 → .zshrc.backup"
+    mv "$HOME/.zshrc" "$HOME/.zshrc.backup"
+fi
+ln -s "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+echo ".zshrc 심볼릭 링크 완료"
