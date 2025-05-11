2b6fe69f480fbc503ae2198168fe09270ee72f2f
simta1 <koreajunho0219@gmail.com>
2025-05-11 17:39:06 +0900

    add install.sh

---
+++ README.md
@@ -0,0 +1,10 @@
+```bash
+git clone https://github.com/simta1/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+chmod +x install.sh
+./install.sh
+```
+이후 nvim 열어서
+```nvim
+:PlugInstall
+```
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOTFILES_DIR="$HOME/dotfiles"
+NVIM_CONFIG_DIR="$HOME/.config/nvim"
+mkdir -p "$NVIM_CONFIG_DIR"
+
+if [ -f "$NVIM_CONFIG_DIR/init.vim" ]; then
+    echo "기존 init.vim 백업 → init.vim.backup"
+    mv "$NVIM_CONFIG_DIR/init.vim" "$NVIM_CONFIG_DIR/init.vim.backup"
+fi
+
+ln -sfn "$DOTFILES_DIR/.config/nvim/init.vim" "$NVIM_CONFIG_DIR/init.vim"
+echo "init.vim 심볼릭 링크 생성 완료"
+
+PLUG_VIM_PATH="$HOME/.local/share/nvim/site/autoload/plug.vim"
+if [ ! -f "$PLUG_VIM_PATH" ]; then
+    echo "vim-plug 설치 중..."
+    curl -fLo "$PLUG_VIM_PATH" --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    echo "vim-plug 설치 완료"
+else
+    echo "vim-plug 이미 설치됨"
+fi
+
+echo "Neovim 열어서 :PlugInstall으로 플러그인 설치"
+
