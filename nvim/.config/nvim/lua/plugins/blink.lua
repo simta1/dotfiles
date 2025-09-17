ea446d633104e1ab4735ec6b9638f09b5e4f51cb
simta1 <koreajunho0219@gmail.com>
2025-09-17 23:32:51 +0900

    refactor: 들여쓰기 수정

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,32 +1,30 @@
 return {
-    {
-        "saghen/blink.cmp",
-        opts = function(_, opts)
-			opts.snippets = { preset = "luasnip" }
+	"saghen/blink.cmp",
+	opts = function(_, opts)
+		opts.snippets = { preset = "luasnip" }
 
-			opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
-				ghost_text = { enabled = false },
-			})
+		opts.completion = vim.tbl_deep_extend("force", opts.completion or {}, {
+			ghost_text = { enabled = false },
+		})
 
-			opts.sources = opts.sources or {}
-            opts.sources.default = { "lsp", "path", "snippets", "buffer" }
+		opts.sources = opts.sources or {}
+		opts.sources.default = { "lsp", "path", "snippets", "buffer" }
 
-            opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
-                preset = "none",
-				["<CR>"] = { "fallback" },
-                ["<Tab>"] = {
-                    function(cmp)
-                        if cmp.is_visible() then
-                            return cmp.accept()
-                        end
-                    end,
-                    "fallback",
-                },
-				["<C-n>"] = { function(cmp) cmp.select_next() end },
-				["<C-p>"] = { function(cmp) cmp.select_prev() end },
-				["<Down>"] = { function(cmp) cmp.select_next() end },
-				["<Up>"]   = { function(cmp) cmp.select_prev() end },
-            })
-        end,
-    },
+		opts.keymap = vim.tbl_deep_extend("force", opts.keymap or {}, {
+			preset = "none",
+			["<CR>"] = { "fallback" },
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.accept()
+					end
+				end,
+				"fallback",
+			},
+			["<C-n>"] = { function(cmp) cmp.select_next() end },
+			["<C-p>"] = { function(cmp) cmp.select_prev() end },
+			["<Down>"] = { function(cmp) cmp.select_next() end },
+			["<Up>"]   = { function(cmp) cmp.select_prev() end },
+		})
+	end,
 }
