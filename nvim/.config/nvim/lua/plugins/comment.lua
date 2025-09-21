eb7313dc8013424d42d7adde6d0cc58015139fe2
simta1 <koreajunho0219@gmail.com>
2025-09-22 02:17:02 +0900

    feat(nvim): nvim-ts-context-commentstring 플러그인 추가

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,33 +1,51 @@
 return {
-    "numToStr/Comment.nvim",
-    keys = {
-        { "⎘", mode = "n", function()
-            require("Comment.api").toggle.linewise.current()
-        end, desc = "Toggle comment (line)" },
-        { "⎘", mode = "x", function()
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Plug>(comment_toggle_linewise_visual)gv", true, false, true), "x", false)
-        end, desc = "Toggle comment (visual)" },
-		{ "⎘", mode = "i", function()
-			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
-			require("Comment.api").toggle.linewise.current()
-			vim.api.nvim_feedkeys("A", "n", false)
-		end, desc = "Toggle comment (insert)" },
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = true,
+	},
+	{
+		"numToStr/Comment.nvim",
+		keys = {
+			{ "⎘", mode = "n", function()
+				require("Comment.api").toggle.linewise.current()
+			end, desc = "Toggle comment (line)" },
+			{ "⎘", mode = "x", function()
+				vim.api.nvim_feedkeys(
+					vim.api.nvim_replace_termcodes("<Plug>(comment_toggle_linewise_visual)gv", true, false, true),
+					"x",
+					false
+				)
+			end, desc = "Toggle comment (visual)" },
+			{ "⎘", mode = "i", function()
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+				require("Comment.api").toggle.linewise.current()
+				vim.api.nvim_feedkeys("A", "n", false)
+			end, desc = "Toggle comment (insert)" },
 
-		-- neovide 전용
-		{ "<C-/>", mode = "n", function()
-            require("Comment.api").toggle.linewise.current()
-        end, desc = "Toggle comment (line)" },
-        { "<C-/>", mode = "x", function()
-            vim.api.nvim_feedkeys(
-                vim.api.nvim_replace_termcodes("<Plug>(comment_toggle_linewise_visual)gv", true, false, true),
-                "x",
-                false
-            )
-        end, desc = "Toggle comment (visual)" },
-		{ "<C-/>", mode = "i", function()
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
-            require("Comment.api").toggle.linewise.current()
-            vim.api.nvim_feedkeys("A", "n", false)
-        end, desc = "Toggle comment (insert, neovide)" },
-    },
+			-- neovide 전용
+			{ "<C-/>", mode = "n", function()
+				require("Comment.api").toggle.linewise.current()
+			end, desc = "Toggle comment (line)" },
+			{ "<C-/>", mode = "x", function()
+				vim.api.nvim_feedkeys(
+					vim.api.nvim_replace_termcodes("<Plug>(comment_toggle_linewise_visual)gv", true, false, true),
+					"x",
+					false
+				)
+			end, desc = "Toggle comment (visual)" },
+			{ "<C-/>", mode = "i", function()
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", false)
+				require("Comment.api").toggle.linewise.current()
+				vim.api.nvim_feedkeys("A", "n", false)
+			end, desc = "Toggle comment (insert, neovide)" },
+		},
+		dependencies = {
+			"JoosepAlviste/nvim-ts-context-commentstring",
+		},
+		config = function()
+			require("Comment").setup {
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			}
+		end,
+	},
 }
