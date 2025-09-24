53c002b773d30cbf1bda8bd9c188e7e93523745f
simta1 <koreajunho0219@gmail.com>
2025-09-25 00:56:14 +0900

    feat(sway): 600초동안 유휴 상태 유지되면 화면 자동꺼짐

---
+++ bin/.local/bin/lock.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+exec swaylock \
+    --clock --indicator \
+    --hide-keyboard-layout \
+    --indicator-radius 90 \
+    --indicator-thickness 8 \
+    --font-size 36 \
+    --indicator-x-position 140 \
+    --indicator-y-position 1060 \
+    --image eDP-1:~/Pictures/wallpapers/bg_lock.jpg \
+    --image HDMI-A-1:~/Pictures/wallpapers/bg_lock.jpg
