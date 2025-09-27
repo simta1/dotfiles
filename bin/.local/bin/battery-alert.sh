c027fc17bb63576058fb7c10e96562d4857728cf
simta1 <koreajunho0219@gmail.com>
2025-09-28 00:52:36 +0900

    feat: 배터리 80% 이상에서 충전 중일 경우 알림

--- bin/.local/bin/battery-alert.sh
+++ bin/.local/bin/battery-alert.sh
@@ -7,6 +7,7 @@ STATUS_PATH="/sys/class/power_supply/BAT0/status"
 
 WARN=20
 CRIT=10
+FULL=80
 
 capacity=$(<"$BATTERY_PATH")
 status=$(<"$STATUS_PATH")
@@ -18,5 +19,10 @@ if [[ "$status" == "Discharging" ]]; then
     elif (( capacity <= WARN )); then
         notify-send -u normal -i ~/.local/bin/icons/battery-low.svg \
             "Battery Notice" "<span font='16' foreground='#fb8c00'><b>Battery at ${capacity}%</b></span>"
+    fi
+elif [[ "$status" == "Charging" ]]; then
+    if (( capacity >= FULL )); then
+        notify-send -u normal -i ~/.local/bin/icons/battery-full.svg \
+			"Battery Notice" "<span font='16' foreground='#43a047'><b>Battery ${capacity}%. Consider unplugging.</b></span>"
     fi
 fi
