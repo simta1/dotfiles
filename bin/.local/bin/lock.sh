be2e53d38be69811729ab5adc4cbe5ce22b71474
simta1 <koreajunho0219@gmail.com>
2025-10-06 22:54:21 +0900

    feat(swaylock): 잠금시도 실패 회수 표시

--- bin/.local/bin/lock.sh
+++ bin/.local/bin/lock.sh
@@ -7,6 +7,8 @@ exec swaylock \
 	--indicator-thickness 8 \
 	--indicator-x-position 140 \
 	--indicator-y-position 1060 \
+	--indicator-image ~/Pictures/wallpapers/bg_space.jpg \
 	--hide-keyboard-layout \
+	--show-failed-attempts \
 	--image eDP-1:~/Pictures/wallpapers/bg_lock.jpg \
 	--image HDMI-A-1:~/Pictures/wallpapers/bg_lock.jpg
