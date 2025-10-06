0a35b51083cea5c6cc2e41e8c3b311f523b3b868
simta1 <koreajunho0219@gmail.com>
2025-10-06 10:02:59 +0900

    chore: 안쓰는 옛날 코드들 제거

--- waybar/.config/waybar/scripts/dpms_status.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# 출력들 중 하나라도 dpms 꺼져있으면 OFF, 모두 켜져있으면 ON
-if swaymsg -t get_outputs -r | jq -e '[.[] | select(.active==true) | .dpms // true] | all' >/dev/null; then
-  echo " ON"
-else
-  echo " OFF"
-fi
--- waybar/.config/waybar/scripts/dpms_toggle.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-if swaymsg -t get_outputs -r | jq -e '[.[] | select(.active==true) | .dpms // true] | all' >/dev/null; then
-  swaymsg 'output * dpms off' >/dev/null
-else
-  swaymsg 'output * dpms on'  >/dev/null
-fi
