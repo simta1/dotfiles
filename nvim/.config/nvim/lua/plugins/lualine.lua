78496745ede54abd96b3881d98a73935e2f321a7
simta1 <koreajunho0219@gmail.com>
2025-12-19 00:44:32 +0900

    feat(nvim): lualine에 파일 크기 표시

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,11 +1,29 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	enabled = true,
-	opts = {
-		sections = {
-			lualine_c = {
-				{ "filename", path = 1, symbols = { modified = " [+]", readonly = " [-]" } },
-			},
-		},
-	},
+    "nvim-lualine/lualine.nvim",
+    enabled = true,
+    opts = {
+        sections = {
+            lualine_c = {
+                {
+                    "filename",
+                    path = 1,
+                    symbols = { modified = "[+]", readonly = "[-]" },
+                    separator = { right = "" },
+                    color = { bg = "#3d59a1", },
+                },
+                {
+                    function()
+                        local size = vim.fn.wordcount().bytes
+                        if size <= 0 then return "" end
+                        local format_size = size < 20000 and size .. "B" 
+                        or size < 1024 * 1024 and string.format("%.1fKB", size / 1024)
+                        or string.format("%.1fMB", size / (1024 * 1024))
+                        return format_size
+                    end,
+                    separator = { right = "" },
+                    color = { bg = "#708090", },
+                },
+            },
+        },
+    },
 }
