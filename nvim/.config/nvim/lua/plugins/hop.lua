9d231531cbdc50eb1a37edf44bd055986fe2afeb
simta1 <koreajunho0219@gmail.com>
2026-02-25 20:49:41 +0900

    feat(nvim): hop.nvim 단축키 추가

--- nvim/.config/nvim/lua/plugins/hop.lua
+++ nvim/.config/nvim/lua/plugins/hop.lua
@@ -6,6 +6,7 @@ return {
 			hop.setup()
 
 			vim.keymap.set("n", "<leader><leader>w", "<cmd>HopWord<CR>", { desc = "Hop by word" })
+			vim.keymap.set("n", "\\", "<cmd>HopWord<CR>", { desc = "Hop by word" })
 			-- vim.keymap.set("n", "<leader><leader>f", "<cmd>HopChar1<CR>", { desc = "Hop by single char" })
 			-- vim.keymap.set("n", "<leader><leader>l", "<cmd>HopLine<CR>", { desc = "Hop by line" })
 			-- vim.keymap.set("n", "<leader><leader>p", "<cmd>HopPattern<CR>", { desc = "Hop by pattern" })
