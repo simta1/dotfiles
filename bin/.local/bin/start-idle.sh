92921bd4c33780e83e1b0d2af93d51519256123c
simta1 <koreajunho0219@gmail.com>
2026-03-14 04:20:17 +0900

    fix: swaymsg대신 hyprctl 사용

--- bin/.local/bin/start-idle.sh
+++ bin/.local/bin/start-idle.sh
@@ -3,6 +3,6 @@ swayidle -w \
     timeout 510 'notify-send "Idle" "화면 자동꺼짐까지 90초"' \
     timeout 540 'notify-send "Idle" "화면 자동꺼짐까지 60초"' \
     timeout 570 'notify-send "Idle" "화면 자동꺼짐까지 30초"' \
-    timeout 600 '~/.local/bin/lock.sh & swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' \
-    before-sleep '~/.local/bin/lock.sh'
+    timeout 600 'hyprlock & hyprctl dispatch dpms off' \
+    resume 'hyprctl dispatch dpms on' \
+    before-sleep 'hyprlock'
