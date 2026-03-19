aa940f085ba355b65c04241d08a707f8eba41cf3
simta1 <koreajunho0219@gmail.com>
2026-03-19 13:49:44 +0900

    refactor(nvim): cin.nvim 플러그인으로 변경

---
+++ nvim/.config/nvim/lua/plugins/cin.lua
@@ -0,0 +1,13 @@
+return {
+    -- dir = "~/coding/my/cin.nvim",
+    "simta1/cin.nvim",
+    ft = "cpp",
+    opts = {
+        mapping = "<leader>in",
+        types = {
+            "int", "float", "double", "char", "bool", "string", "std::string",
+            "long long", "long double",
+            "ll", "ld", "ull"
+        }
+    },
+}
