289abc8ea70ca2d814b759d05059a8cf7f585719
simta1 <koreajunho0219@gmail.com>
2026-03-27 20:56:14 +0900

    feat(nvim): barbar BufferInactiveMod, BufferCurrent색 변경

--- nvim/.config/nvim/lua/plugins/barbar.lua
+++ nvim/.config/nvim/lua/plugins/barbar.lua
@@ -1,76 +1,98 @@
 return {
-	"romgrk/barbar.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-		"lewis6991/gitsigns.nvim",
-	},
-	opts = {
-		diagnostics = {
-			-- [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "" },
-			-- [vim.diagnostic.severity.WARN]  = { enabled = true, icon = "" },
-			[vim.diagnostic.severity.ERROR] = { enabled = false },
-			[vim.diagnostic.severity.WARN]  = { enabled = false },
-			[vim.diagnostic.severity.INFO]  = { enabled = false },
-			[vim.diagnostic.severity.HINT]  = { enabled = false },
-		},
-		icons = {
-			buffer_index = true,
-			filetype = { enabled = true },  -- 수정
-			button = "×",
-		},
-		-- close_command = function(bufnr)
-		-- 	vim.api.nvim_buf_delete(bufnr, {})
-		-- end
+    "romgrk/barbar.nvim",
+    event = "VeryLazy",
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+        "lewis6991/gitsigns.nvim",
+    },
+    opts = {
+        diagnostics = {
+            -- [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "" },
+            -- [vim.diagnostic.severity.WARN]  = { enabled = true, icon = "" },
+            [vim.diagnostic.severity.ERROR] = { enabled = false },
+            [vim.diagnostic.severity.WARN]  = { enabled = false },
+            [vim.diagnostic.severity.INFO]  = { enabled = false },
+            [vim.diagnostic.severity.HINT]  = { enabled = false },
+        },
+        icons = {
+            buffer_index = true,
+            filetype = {
+                enabled = true,
+                custom_colors = true
+            },
+            button = "×",
+        },
+        -- close_command = function(bufnr)
+        -- 	vim.api.nvim_buf_delete(bufnr, {})
+        -- end
 
-		-- Close commands
-		-- 	:BufferCloseAllButCurrent
-		-- 	:BufferCloseAllButPinned
-		-- 	:BufferCloseAllButCurrentOrPinned
-		-- 	:BufferCloseBuffersLeft
-		-- 	:BufferCloseBuffersRight
-	},
-	keys = {
-		{ "<S-H>", "<Cmd>BufferPrevious<CR>", desc = "Prev buffer (barbar)" },
-		{ "<S-L>", "<Cmd>BufferNext<CR>", desc = "Next buffer (barbar)" },
-	},
-	config = function(_, opts)
-		require("barbar").setup(opts)
+        -- Close commands
+        -- 	:BufferCloseAllButCurrent
+        -- 	:BufferCloseAllButPinned
+        -- 	:BufferCloseAllButCurrentOrPinned
+        -- 	:BufferCloseBuffersLeft
+        -- 	:BufferCloseBuffersRight
+    },
+    keys = {
+        { "<S-H>", "<Cmd>BufferPrevious<CR>", desc = "Prev buffer (barbar)" },
+        { "<S-L>", "<Cmd>BufferNext<CR>", desc = "Next buffer (barbar)" },
+    },
+    config = function(_, opts)
+        require("barbar").setup(opts)
 
-		local map = vim.keymap.set
+        local hl = vim.api.nvim_set_hl
+        local bg_color = "#191a21"
+        local yellow_bright = "#F1FA8C"
+        local yellow_dim = "#8d915e"
+        local cyan_bright = "#8BE9FD"
 
-		-- Move to previous/next
-		map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
-		map("n", "<A-.>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+        -- hl(0, "BufferTabpageFill", { bg = "NONE" })
+        hl(0, "BufferTabpageFill", { bg = bg_color })
 
-		-- 여기에 설정하면 lazyvim기본 매핑(bnext, bprevious)에 덮어씌워져서 keys={}으로 옮김
-		-- map("n", "<S-H>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
-		-- map("n", "<S-L>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
+        hl(0, "BufferCurrent", { fg = cyan_bright, bg = bg_color, bold = true })
+        hl(0, "BufferCurrentIndex", { fg = cyan_bright, bg = bg_color })
+        hl(0, "BufferCurrentIcon", { fg = cyan_bright, bg = bg_color })
+        hl(0, "BufferCurrentSign", { fg = cyan_bright, bg = bg_color })
+        hl(0, "BufferCurrentTarget", { fg = cyan_bright, bg = bg_color })
+        hl(0, "BufferCurrentBtn", { fg = cyan_bright, bg = bg_color })
 
-		-- Re-order to previous/next
-		map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", { noremap = true, silent = true })
-		map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", { noremap = true, silent = true })
+        hl(0, "BufferCurrentMod", { fg = yellow_bright, bg = bg_color, bold = true })
+        hl(0, "BufferInactiveMod", { fg = yellow_dim, bg = bg_color })
 
-		-- Goto buffer in position...
-		map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { noremap = true, silent = true })
-		map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', { noremap = true, silent = true })
-		map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', { noremap = true, silent = true })
-		map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', { noremap = true, silent = true })
-		map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', { noremap = true, silent = true })
-		map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', { noremap = true, silent = true })
-		map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', { noremap = true, silent = true })
-		map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', { noremap = true, silent = true })
-		map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', { noremap = true, silent = true })
-		map("n", "<A-0>", "<Cmd>BufferLast<CR>", { noremap = true, silent = true })
+        local map = vim.keymap.set
 
-		-- Pin/unpin buffer
-		-- map("n", "<A-p>", "<Cmd>BufferPin<CR>", { noremap = true, silent = true }) -- competitest에서 A-p사용중
+        -- Move to previous/next
+        map("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
+        map("n", "<A-.>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
 
-		-- Goto pinned/unpinned buffer
-		-- 	:BufferGotoPinned
-		-- 	:BufferGotoUnpinned
+        -- 여기에 설정하면 lazyvim기본 매핑(bnext, bprevious)에 덮어씌워져서 keys={}으로 옮김
+        -- map("n", "<S-H>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true })
+        -- map("n", "<S-L>", "<Cmd>BufferNext<CR>", { noremap = true, silent = true })
 
-		-- Close buffer
-		map("n", "<A-c>", "<Cmd>BufferClose<CR>", { noremap = true, silent = true })
-	end,
+        -- Re-order to previous/next
+        map("n", "<A-<>", "<Cmd>BufferMovePrevious<CR>", { noremap = true, silent = true })
+        map("n", "<A->>", "<Cmd>BufferMoveNext<CR>", { noremap = true, silent = true })
+
+        -- Goto buffer in position...
+        map('n', '<A-1>', '<Cmd>BufferGoto 1<CR>', { noremap = true, silent = true })
+        map('n', '<A-2>', '<Cmd>BufferGoto 2<CR>', { noremap = true, silent = true })
+        map('n', '<A-3>', '<Cmd>BufferGoto 3<CR>', { noremap = true, silent = true })
+        map('n', '<A-4>', '<Cmd>BufferGoto 4<CR>', { noremap = true, silent = true })
+        map('n', '<A-5>', '<Cmd>BufferGoto 5<CR>', { noremap = true, silent = true })
+        map('n', '<A-6>', '<Cmd>BufferGoto 6<CR>', { noremap = true, silent = true })
+        map('n', '<A-7>', '<Cmd>BufferGoto 7<CR>', { noremap = true, silent = true })
+        map('n', '<A-8>', '<Cmd>BufferGoto 8<CR>', { noremap = true, silent = true })
+        map('n', '<A-9>', '<Cmd>BufferGoto 9<CR>', { noremap = true, silent = true })
+        map("n", "<A-0>", "<Cmd>BufferLast<CR>", { noremap = true, silent = true })
+
+        -- Pin/unpin buffer
+        -- map("n", "<A-p>", "<Cmd>BufferPin<CR>", { noremap = true, silent = true }) -- competitest에서 A-p사용중
+
+        -- Goto pinned/unpinned buffer
+        -- 	:BufferGotoPinned
+        -- 	:BufferGotoUnpinned
+
+        -- Close buffer
+        map("n", "<A-c>", "<Cmd>BufferClose<CR>", { noremap = true, silent = true })
+    end,
 }
