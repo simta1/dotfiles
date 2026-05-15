14a9e1f6478320fb9bea00e4dd8ed57050f82c5d
simta1 <koreajunho0219@gmail.com>
2026-05-15 20:19:04 +0900

    feat: TLP

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -9,6 +9,19 @@
   hardware.bluetooth.powerOnBoot = true;
   services.blueman.enable = true;
 
+  services.tlp = {
+    enable = true;
+    settings = {
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    };
+  };
+
+  services.power-profiles-daemon.enable = false;
+
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # Bootloader.
