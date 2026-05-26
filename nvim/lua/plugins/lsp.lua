aa1a212d03b5cc5e8b0047030bd4614ade758a6d
simta1 <koreajunho0219@gmail.com>
2026-05-26 13:16:26 +0900

    fix(nvim): mason대신 nix clangd 사용

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,18 +4,14 @@ return {
         inlay_hints = { enabled = false },
         servers = {
             clangd = {
+                cmd = {
+                    "/etc/profiles/per-user/simtal/bin/clangd",
+                    "--query-driver=/etc/profiles/per-user/simtal/bin/g++,/nix/store/*gcc-wrapper*/bin/g++",
+                    "--log=verbose",
+                },
                 single_file_support = true,
-                root_dir = function(fname)
-                    local util = require("lspconfig.util")
-                    local path = (type(fname) == "number") and vim.api.nvim_buf_get_name(fname) or fname
-                    if not path or path == "" then
-                        return vim.uv.cwd()
-                    end
-                    local root = util.root_pattern(".clangd", "compile_commands.json", "compile_flags.txt", ".git")(path)
-                    return root or vim.fs.dirname(path) or vim.uv.cwd()
-                end,
             },
-            stylua = false,
+            stylua = { enabled = false },
         },
     },
 }
